93b6ff7da77cc6cab821583b9a729c4b3aaa9405
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2023-01-08 19:02:09 +0200

    created the login and signup template

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,14 +2,17 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.render('homepage', {
-      id: 1,
-      title: 'Handlebars Docs',
-      created_at: new Date(),
-      comments: [{}, {}],
-      user: {
-        username: 'test_user'
-      }
+        id: 1,
+        title: 'Handlebars Docs',
+        created_at: new Date(),
+        user: {
+            username: 'test_user'
+        }
     });
-  });
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
 
 module.exports = router;
