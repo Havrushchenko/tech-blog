7410bf1b113af0246834ce0ce1c640f2391d03bc
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2023-01-09 22:07:15 +0200

    added logout router

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,38 +1,15 @@
-async function signupFormHandler(event) {
-    event.preventDefault();
-
-    const password = document.querySelector('#password-signup').value.trim();
-
-    if (username && password) {
-        const response = await fetch('/api/users', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-        // check the response status
-        if (response.ok) {
-            console.log('success');
-        } else {
-            alert(response.statusText);
-        }
-    }
-}
-
 async function loginFormHandler(event) {
     event.preventDefault();
 
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const username = document.querySelector("#username-login");
+    const password = document.querySelector("#password-login");
 
-    if (email && password) {
+    if (username && password) {
         const response = await fetch('/api/users/login', {
             method: 'post',
             body: JSON.stringify({
-                email,
-                password
+                username: username,
+                password: password
             }),
             headers: { 'Content-Type': 'application/json' }
         });
@@ -45,5 +22,4 @@ async function loginFormHandler(event) {
     }
 }
 
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector("#login-form").addEventListener("submit", loginFormHandler);
